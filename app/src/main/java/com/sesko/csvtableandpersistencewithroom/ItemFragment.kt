91f7f5219a66973f7fdcb525b6c0d1d813deb38c
Daniel <daniel@kollar-web.de>
2023-09-28 07:17:20 +0200

    Add table header

--- app/src/main/java/com/sesko/csvtableandpersistencewithroom/ItemFragment.kt
+++ app/src/main/java/com/sesko/csvtableandpersistencewithroom/ItemFragment.kt
@@ -2,7 +2,6 @@ package com.sesko.csvtableandpersistencewithroom
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
-import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.DividerItemDecoration
@@ -10,11 +9,16 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.sesko.csvtableandpersistencewithroom.placeholder.PlaceholderContent
+import com.sesko.csvtableandpersistencewithroom.databinding.FragmentItemListBinding
 
 /**
  * A fragment representing a list of Items.
  */
 class ItemFragment : Fragment() {
+
+    private var _binding: FragmentItemListBinding? = null
+    private val binding get() = _binding!!
+    private lateinit var recyclerView: RecyclerView
 
     private var columnCount = 3
 
@@ -30,16 +34,20 @@ class ItemFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val view = inflater.inflate(R.layout.fragment_item_list, container, false)
+        _binding = FragmentItemListBinding.inflate(inflater, container, false)
+        val view = binding.root
+        recyclerView = binding.recyclerView
 
         // Set the adapter
-        if (view is RecyclerView) {
-            with(view) {
-                view.addItemDecoration(DividerItemDecoration(context,
-                    DividerItemDecoration.VERTICAL))
-                layoutManager = LinearLayoutManager(context)
-                adapter = MyItemRecyclerViewAdapter(PlaceholderContent.ITEMS)
-            }
+        with(recyclerView) {
+            recyclerView.addItemDecoration(
+                DividerItemDecoration(
+                    context,
+                    DividerItemDecoration.VERTICAL
+                )
+            )
+            layoutManager = LinearLayoutManager(context)
+            adapter = MyItemRecyclerViewAdapter(PlaceholderContent.ITEMS)
         }
         return view
     }
